b61882d90d824168d2a5f70fbba4191abab6d085
Roman Krysinski <rkrysinski@gmail.com>
2016-05-17 23:08:07 +0200

    Close #1 problem with firefox invalid date displayed.

--- oilprice-frontend/src/main/resources/static/common/oilprice/converter.js
+++ oilprice-frontend/src/main/resources/static/common/oilprice/converter.js
@@ -10,11 +10,13 @@ angular
 			var pricePerLiterWithMargin = quantityPricePerLiter * 1.15;
 			return Number((pricePerLiterWithMargin).toFixed(2));
 		}
-		
+
 		function getDateFor(oilPrice) {
-			var isoFormatInput = oilPrice.date.year + "-" + oilPrice.date.month + "-" + oilPrice.date.day;
-			var date = new Date(isoFormatInput);
-			return date.toLocaleDateString();
+			return getDateObjFor(oilPrice).toLocaleDateString();
+		}
+		
+		function getDateObjFor(oilPrice) {
+			return new Date(oilPrice.date.year, oilPrice.date.month, oilPrice.date.day);
 		}
 		
 		function getCurrentyOf(oilPrice) {
